d2c574309995a1a400b06a3f3b39c7703438d314
ysagoglu90 <yigitss_2006@hotmail.com>
2021-06-15 21:33:02 +0300

    float addition service added

--- src/main/java/com/metu/sm703/CollectorService.java
+++ src/main/java/com/metu/sm703/CollectorService.java
@@ -23,6 +23,12 @@ public class CollectorService {
 		return number1 + number2;
 	}
 	
+	@RequestMapping("/collect/first/{first}/second/{second}")
+	public float collect(@PathVariable float first, @PathVariable float second) {
+		
+		return first + second;
+	}
+	
 	@RequestMapping("/collectstring/first/{first}/second/{second}")
 	public String collectstring(@PathVariable String first, @PathVariable String second) {
 		
